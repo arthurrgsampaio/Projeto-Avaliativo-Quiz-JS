6c0ab793cc5c29898a5e039288e46efc8def4adf
VitorLopes02 <vitinhoolopes.vl@gmail.com>
2023-07-24 19:57:34 -0300

    added-questions

---
+++ scripts/esportes.js
@@ -0,0 +1,57 @@
+export const esportes = [
+    {
+        titulo: "Qual é o esporte mais popular do mundo?",
+        alternativas: ["Futebol", "Basquete", "Críquete", "Vôlei"],
+        correctAlternativa: 0,
+    },
+    {
+        titulo: "Quantos jogadores tem uma equipe de futebol?",
+        alternativas: ["11", "3", "15", "9"],
+        correctAlternativa: 0,
+    },
+    {
+        titulo: "Qual é o esporte olímpico mais antigo?",
+        alternativas: ["Luta livre", "Boxe", "Atletismo", "Ginástica"],
+        correctAlternativa: 2,
+    },
+    {
+        titulo: "Qual é o esporte mais caro do mundo?",
+        alternativas: ["Golfe", "Fórmula 1", "Polo", "Yachting"],
+        correctAlternativa: 3,
+    },
+    {
+        titulo: "Qual é o esporte mais popular do Brasil?",
+        alternativas: ["Basquete", "Vôlei", "Futebol", "Natação"],
+        correctAlternativa: 2,
+    },
+    {
+        titulo: "Qual é o esporte mais popular dos Estados Unidos?",
+        alternativas: ["Futebol americano", "Basquete", "Beisebol", "Hóquei no gelo"],
+        correctAlternativa: 0,
+    },
+    {
+        titulo: "Qual é o esporte mais popular da China?",
+        alternativas: ["Basquete", "Vôlei", "Futebol", "Natação"],
+        correctAlternativa: 2,
+    },
+    {
+        titulo: "Qual é o esporte mais popular da Índia?",
+        alternativas: ["Basquete", "Kabaddi", "Cricket", "Natação"],
+        correctAlternativa: 2,
+    },
+    {
+        titulo: " Que país é conhecido por criar o esporte do judô?",
+        alternativas: ["Irlanda", "China", "Coreia do Sul", "Japão."],
+        correctAlternativa: 3,
+    },
+    {
+        titulo: "Qual esporte é conhecido como o esporte mais rápido do mundo?",
+        alternativas: ["Basquete", "Kabaddi", "Cricket", "Natação"],
+        correctAlternativa: 3,
+    }
+];
+
+
+
+
+
