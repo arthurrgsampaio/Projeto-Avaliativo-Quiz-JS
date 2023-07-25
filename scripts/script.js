62d9bef4f38faac7e6fc4d52b0aeebc231029a7e
arthurrgsampaio <arthur.rgs03@gmail.com>
2023-07-25 11:14:38 -0300

    if acrscentado

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,58 @@
 import { entretenimento } from "./entretenimento.js";
-// import { entretenimento } from "./entretenimento.js";
-// import { esportes } from "./esportes.js";
+import { artes } from "./artes.js";
+import { esportes } from "./esportes.js";
 
 const nome = document.getElementById("name");
+
 const tema = document.getElementById("temas");
+let selectedTema;
+tema.addEventListener("change", () => {
+  selectedTema = tema.value;
+});
+let valores = [];
+
 const btnIniciar = document.getElementById("btn-iniciar");
 const listaPerguntas = document.getElementById("lista-perguntas");
-const question = document.querySelector("entretenimento");
+const containerInicial = document.querySelector(".init-container");
 
-function mostrarTema(entretenimento) {
-    for(let entreteniment of entretenimento) {
-        listaPerguntas.innerHTML += `
+function mostrarTema(values) {
+  let contador = 0;
+  containerInicial.style = "display: none";
+  for (let value of values) {
+    listaPerguntas.innerHTML += `
             <li class="perguntas-container">
-            <div class="e-titulo">${entreteniment.titulo}</div>
+            <div class="e-titulo">${value.titulo}</div>
             <div class="lista-organizacao">
-                <input type="radio" name="select" value="0"><label>${entreteniment.alternativas[0]}</label>
-                <input type="radio" name="select" value="1"><label>${entreteniment.alternativas[1]}</label>
-                <input type="radio" name="select" value="2"><label>${entreteniment.alternativas[2]}</label>
-                <input type="radio" name="select" value="3"><label>${entreteniment.alternativas[3]}</label>
+                <input type="radio" name="${
+                  "select" + contador
+                }" value="0"><label>${value.alternativas[0]}</label>
+                <input type="radio" name="${
+                  "select" + contador
+                }" value="1"><label>${value.alternativas[1]}</label>
+                <input type="radio" name="${
+                  "select" + contador
+                }" value="2"><label>${value.alternativas[2]}</label>
+                <input type="radio" name="${
+                  "select" + contador
+                }" value="3"><label>${value.alternativas[3]}</label>
             </div>
             </li>
         `;
-    }
+    contador++;
+  }
 }
 
-btnIniciar.addEventListener("click", mostrarTema(entretenimento));
+btnIniciar.addEventListener("click", () => {
+  if (selectedTema === "entreterimento") {
+    valores = entretenimento;
+    mostrarTema(valores);
+  } else if (selectedTema === "artes") {
+    valores = artes;
+    mostrarTema(valores);
+  } else if (selectedTema === "esportes") {
+    valores = esportes;
+    mostrarTema(valores);
+  } else {
+    console.log(`Não funcionou...`);
+  }
+});
